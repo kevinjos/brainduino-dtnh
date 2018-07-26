69b31c176ae208280248b0dd2c0e2e52aa1255d4
noisebridge <noisebridge@alpine-unicorn.noise>
2018-07-25 22:25:50 -0700

    fix up the python code to work with a real brainduino!

--- brainduino-web-ui/main.py
+++ brainduino-web-ui/main.py
@@ -1,60 +1,9 @@
-import random
-import time
+import asyncio
 
-from flask import Flask, render_template
-from flask_socketio import SocketIO, send
-
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
-
-
-class MockBrainduino:
-
-    def __init__(self, tty='/dev/null', baud=256000):
-        self.tty = tty
-        self.baud = baud
-        self.data_stream_gen = self._data_stream_gen()
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, *args):
-        self.data_stream_gen.close()
-        return
-
-    def read(self, nbytes):
-        s = ''
-        for i in range(nbytes):
-            s += next(self.data_stream_gen)
-        return s
-
-    def _data_stream_gen(self):
-        """
-        Mock brainduino output:
-        DEADBE\tEF0123\n
-        DEADBE\tEF0123\n
-        DEADBE\tEF0123\n
-        """
-        hex_ctr = 0
-        s = ''
-        while 1:
-            if hex_ctr == 6:
-                s = '\t'
-                hex_ctr += 1
-            elif hex_ctr == 13:
-                s = '\n'
-                hex_ctr = 0
-                socketio.sleep(0.004)
-            else:
-                s = random.choice('0123456789ABCDEF')
-                hex_ctr += 1
-            yield s
-
+import serial
+import websockets
 
 def offsetBinaryToInt(hexstr, offset):
-    # Perhaps we should test that the offset makes sense give the length of the hex string?
     if len(hexstr) != 6:
         return -1
     not_encoded = int(hexstr, 16)  # Make a python integer out of the hex string
@@ -64,28 +13,39 @@ def offsetBinaryToInt(hexstr, offset):
         encoded = not_encoded - 2**offset
     return encoded
 
+def adc2volts(i):
+    return i * 5 / 2**23
 
-b = MockBrainduino()
+async def brains(websocket, path):
+    """
+    Example Brainduino serial stream:
 
+    F80F19\t7801F4\rF80F19\t7801F4\rF80F19\t7801F4\r
+    """
+    s = serial.Serial("/dev/rfcomm0", baudrate=230400)
+    s.flush()
+    channels = (bytearray(6), bytearray(6))
+    hex_ctr = 0
+    while True:
+        bs = s.read(64)
+        for b in bs:
+            if b == ord('\r'):
+                try:
+                    c1, c2 = offsetBinaryToInt(channels[0], 23), offsetBinaryToInt(channels[1], 23)
+                    c1, c2 = adc2volts(c1), adc2volts(c2)
+                    await websocket.send("%s %s" % (c1, c2))
+                except Exception:
+                    await websocket.send("%s %s" % channels)
+                hex_ctr = 0
+            elif b == ord('\t'):
+                continue
+            elif hex_ctr < 6:
+                channels[0][hex_ctr] = b
+            elif hex_ctr > 6:
+                channels[1][hex_ctr % 6] = b
+            hex_ctr += 1
 
-@socketio.on('connect')
-def test_connect():
-    socketio.send('connected')
+start_server = websockets.serve(brains, '127.0.0.1', 5678)
 
-    def send_data():
-        while 1:
-            s = b.read(14)
-            s = s.strip()
-            d = s.split('\t')
-            d = [offsetBinaryToInt(x, 23) * 5 / 2**23 for x in d]
-            socketio.send(d)
-    socketio.start_background_task(send_data)
-
-
-@app.route('/')
-def hello():
-    return render_template('main.html')
-
-
-if __name__ == '__main__':
-    socketio.run(app)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
